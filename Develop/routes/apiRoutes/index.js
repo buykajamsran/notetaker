740b2a01b0e91a8b9c5683612b7b7a5cc7aebc0a
buykajamsran <buyka.jamsran@gmail.com>
2022-02-13 23:55:08 -0600

    apiRoutes updated and nanoid added

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,5 +1,18 @@
 const { createNewNote, validateNote } = require('../../lib/notes');
 const router = require('express').Router();
 const { notes } = require('../../db/notes');
+import { nanoid } from 'nanoid';
 
+router.post('/notes', (req, res) => {
+
+    req.body.id = nanoid();
+    
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is missing information.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+})
+// export router
 module.exports = router;
